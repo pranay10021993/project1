e68ce3691bdd3b99aef2930fc187d18ccdf12749
pranay10021993 <likharpranay@gmail.com>
2023-03-29 10:43:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.scss';
+import Header from './Component/Header/Header';
+import Body from './Component/Body/Body';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Body/>
+
+    </div>
+  );
+}
+
+export default App;
